feaa86c2b9a7636e9fdab2e59dc3cf5ec7516c27
Shaun <6758505+shaunm@users.noreply.github.com>
2022-04-26 00:01:53 -0400

    Update next-seo.config.ts

--- apps/web/lib/config/next-seo.config.ts
+++ apps/web/lib/config/next-seo.config.ts
@@ -3,8 +3,8 @@ import { DefaultSeoProps } from "next-seo";
 import { HeadSeoProps } from "@components/seo/head-seo";
 
 const seoImages = {
-  default: "https://cal.com/og-image.png",
-  ogImage: "https://og-image-one-pi.vercel.app/",
+  default: "" /*"https://cal.com/og-image.png"*/,
+  ogImage: ""  /*"https://og-image-one-pi.vercel.app/"*/,
 };
 
 export const getSeoImage = (key: keyof typeof seoImages): string => {
@@ -16,7 +16,7 @@ export const seoConfig: {
   defaultNextSeo: DefaultSeoProps;
 } = {
   headSeo: {
-    siteName: "Cal.com",
+    siteName: "Bookshaun.ml",
   },
   defaultNextSeo: {
     twitter: {
