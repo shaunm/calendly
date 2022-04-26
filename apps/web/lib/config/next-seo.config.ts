c4925ca77724a69a0af5312f6d14285c48603720
Shaun <6758505+shaunm@users.noreply.github.com>
2022-04-26 00:11:48 -0400

    Update next-seo.config.ts

--- apps/web/lib/config/next-seo.config.ts
+++ apps/web/lib/config/next-seo.config.ts
@@ -3,8 +3,8 @@ import { DefaultSeoProps } from "next-seo";
 import { HeadSeoProps } from "@components/seo/head-seo";
 
 const seoImages = {
-  default: "" /*"https://cal.com/og-image.png"*/,
-  ogImage: ""  /*"https://og-image-one-pi.vercel.app/"*/,
+  default: "https://cal.com/og-image.png",
+  ogImage: "https://og-image-one-pi.vercel.app/",
 };
 
 export const getSeoImage = (key: keyof typeof seoImages): string => {
